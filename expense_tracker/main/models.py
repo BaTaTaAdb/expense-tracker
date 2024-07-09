d2e546280ead7795e9235015da2e7787c2ab4bd2
BaTaTaAdb <joaomnuno@gmail.com>
2024-07-09 20:31:26 +0100

    added new order models

--- expense_tracker/main/models.py
+++ expense_tracker/main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -16,21 +16,24 @@ class Product(models.Model):
     def __str__(self) -> str:
         return self.name
     
-class ShoppingCart(models.Model):
+class Courier(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image_url = models.URLField(max_length=200, blank=True, null=True)
+    image = models.ImageField(upload_to='couriers/', blank=True, null=True)
+    url = models.URLField(max_length=2083)
+    
+class OrderDelivery(models.Model):
+    order_status = models.TextField(blank=True, null=True)
+    courier = models.ForeignKey(Courier, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+        
+class Order(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return f"Shopping Cart ({self.user.username})"
-    
-class CartItem(models.Model):
-    cart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return f"{self.quantity} of {self.product.name}"
+    status = models.TextField(default="Processing",blank=True, null=True)
+    delivery = models.OneToOneField(OrderDelivery, on_delete=models.CASCADE)
     
