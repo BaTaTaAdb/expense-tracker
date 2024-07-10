b4e0c50d13104c139c02f7aa36996c059f59652d
BaTaTaAdb <joaomnuno@gmail.com>
2024-07-10 12:31:53 +0100

    new views and organization

--- expense_tracker/login_register/views.py
+++ expense_tracker/login_register/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegisterForm
 
-# Create your views here.
+# Login/register views
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('account')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('account')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
